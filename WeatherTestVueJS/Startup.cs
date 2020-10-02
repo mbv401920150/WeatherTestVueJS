15b93df45808136494c13a5b58b10c77b8e9c236
Michael Bolanos Vargas <mbv401920150@outlook.com>
2020-10-02 01:20:40 -0600

    Revert "Test"
    This reverts commit 7f2ed8450d6037d88f1be28b745e50f3a8fd59f7.

--- WeatherTestVueJS/Startup.cs
+++ WeatherTestVueJS/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MediatR;
-using VueCliMiddleware;
 
 namespace WeatherTestVueJS
 {
@@ -50,20 +49,6 @@ namespace WeatherTestVueJS
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-
-                // NOTE: VueCliProxy is meant for developement and hot module reload
-                // NOTE: SSR has not been tested
-                // Production systems should only need the UseSpaStaticFiles() (above)
-                // You could wrap this proxy in either
-                // if (System.Diagnostics.Debugger.IsAttached)
-                // or a preprocessor such as #if DEBUG
-                endpoints.MapToVueCliProxy(
-                    "{*path}",
-                    new Microsoft.AspNetCore.SpaServices.SpaOptions { SourcePath = "wwwroot" },
-                    npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
-                    regex: "Compiled successfully",
-                    forceKill: true
-                );
             });
         }
     }
